135a7a234b447d981b95ea0c7be84b8951ddd6a7
benkdrager <benkdrag@gmail.com>
2025-12-11 12:38:06 +0100

    Update

--- komische_oper.js
+++ komische_oper.js
@@ -1,5 +1,5 @@
 var addressPoints = [
-[52.515833,13.386944, "<b>Komische Oper Berlin seit 1947</b><br>Behrenstraße 55-57</br><br><img width=396 height=600 img src=img/komische_oper.jpg><br>Die Große Freitreppe des Eingangsbereiches.<br><sup>Aufnahmejahr: 2007 - Quelle: wikimedia</sup></br><br><a href=https://de.wikipedia.org/wiki/Komische_Oper_Berlin target=_blank>Wikipediaeintrag</a></br><br><a href=https://explore.gnd.network/gnd/46015-1/relations target=_blank>Relationen GND-Explorer</a></br><br>", "Admiralspalast", "3ec400","music"],
+[52.515833,13.386944, "<b>Komische Oper Berlin seit 1947</b><br>Behrenstraße 55-57</br><br><img width=350 height=500 img src=img/komische_oper.jpg><br>Die Große Freitreppe des Eingangsbereiches.<br><sup>Aufnahmejahr: 2007 - Quelle: wikimedia</sup></br><br><a href=https://de.wikipedia.org/wiki/Komische_Oper_Berlin target=_blank>Wikipediaeintrag</a></br><br><a href=https://explore.gnd.network/gnd/46015-1/relations target=_blank>Relationen GND-Explorer</a></br><br>", "Admiralspalast", "3ec400","music"],
 ];
 
 
