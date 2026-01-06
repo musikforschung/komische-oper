bdaaf6829a28fe2eafc13788a38ded98424e361e
benkdrager <benkdrag@gmail.com>
2026-01-06 11:45:24 +0100

    Update

--- komische_oper.js
+++ komische_oper.js
@@ -1,5 +1,5 @@
 var addressPoints = [
-[52.521625,13.388228, "<b>Alte Komische Oper Berlin 1905 - 1952</b><br>Friedrichstraße 104</br><br><img width=512 height=520 img src=img/alte_komische_oper.png><br>Aussenansicht der Komischen Oper in der Friedrichstaße.<br><sup>Aufnahmejahr: 1906 - Quelle: wikimedia</sup></br><br><a href=https://de.wikipedia.org/wiki/Alte_Komische_Oper_Berlin target=_blank>Wikipediaeintrag</a><br><a href=https://explore.gnd.network/gnd/1224235657/relations target=_blank>Relationen GND-Explorer</a></br><br>", "Komische Oper", "3ec400","music"],
+[52.521625,13.388228, "<b>Komische Oper Berlin 1905 - 1952</b><br>Friedrichstraße 104</br><br>Am 25.08.1917 wurde die Operette Schwarwaldmädel an der KKomischen Oper Berlin uraufgeführt</br><br><img width=512 height=520 img src=img/alte_komische_oper.png><br>Aussenansicht der Komischen Oper in der Friedrichstaße.<br><sup>Aufnahmejahr: 1906 - Quelle: wikimedia</sup></br><br><a href=https://de.wikipedia.org/wiki/Alte_Komische_Oper_Berlin target=_blank>Wikipediaeintrag</a><br><a href=https://explore.gnd.network/gnd/1224235657/relations target=_blank>Relationen GND-Explorer</a></br><br>", "Komische Oper", "3ec400","music"],
 ];
 
 
